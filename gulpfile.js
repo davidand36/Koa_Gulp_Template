3bf950c52b3df4122dddbbd8292333c4d399030e
David M. Anderson <dma@epsilondelta.us>
2018-11-15 23:26:57 +0300

    Added Sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
+/*
+    gulpfile.js
+
+    See https://gulpjs.com/
+    and https://www.npmjs.com/package/del
+    and https://www.npmjs.com/package/gulp-replace
+    and https://www.npmjs.com/package/gulp-sass
+    and https://www.npmjs.com/package/node-sass
+*/
+
 const package = require( './package.json' );
 const { series, parallel, src, dest } = require( 'gulp' );
 const del = require( 'del' );
 const replace = require( 'gulp-replace' );
+const sass = require( 'gulp-sass' );
 
 function cleanPublic( ) {
     return del( [ 'public/' ] );
@@ -13,4 +24,13 @@ function processHtml( ) {
         .pipe( dest( 'public/' ) );
 }
 
-exports.default = series( cleanPublic, processHtml );
+function processCss( ) {
+    return src( 'client/**/*.scss' )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( dest( 'public/' ) );
+}
+
+const processClient = parallel( processHtml, processCss );
+
+exports.clean = cleanPublic;
+exports.default = series( cleanPublic, processClient );
