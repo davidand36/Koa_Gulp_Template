dfb094bd364bf0cb8bc8f5d2f8953a92312fa3be
David M. Anderson <dma@epsilondelta.us>
2018-12-20 18:01:12 -0800

    Added Koa Helmet middleware

--- server/web.js
+++ server/web.js
@@ -5,6 +5,7 @@
 
     See https://www.npmjs.com/package/dotenv
     and https://koajs.com/
+    and https://www.npmjs.com/package/koa-helmet
     and https://www.npmjs.com/package/koa-favicon
     and https://www.npmjs.com/package/koa-logger
     and https://www.npmjs.com/package/koa-compress
@@ -13,6 +14,7 @@
 
 require( 'dotenv' ).config( );
 const Koa = require( 'koa' );
+const helmet = require( 'koa-helmet' );
 const favicon = require( 'koa-favicon' );
 const logger = require( 'koa-logger' );
 const compress = require( 'koa-compress' );
@@ -20,6 +22,7 @@ const static = require( 'koa-static' );
 
 const koa = new Koa( );
 
+koa.use( helmet( ) );
 koa.use( favicon( __dirname + '/public/favicon.ico' ) );
 koa.use( logger( ) );
 koa.use( compress( ) );
